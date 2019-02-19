63229ac579c909b38a74cb07a02043ed252f6cb6
Cully Curwen <c.t.g.curwen@gmail.com>
2019-02-19 17:15:57 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop (array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      array[array.length + 1] = "I am 1 strange loops."
+      array[array.length + i] = "I am 1 strange loops."
     }
     else {
-      array[array.length + 1] = "I am ${i} strange loops."
+      array[array.length + i] = "I am ${i} strange loops."
     }
   }
   return array
