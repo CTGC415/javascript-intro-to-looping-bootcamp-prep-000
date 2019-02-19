03617e1ba320b73c177d3364161c7768a0bcbf17
Cully Curwen <c.t.g.curwen@gmail.com>
2019-02-19 17:32:04 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -22,11 +22,14 @@ function whileLoop(number) {
 
 function doWhileLoop(integer) {
   var i = 0
+  
   function incrementVariable() {
-  i = i + 1;
-  return i;
-}
+    i = i + 1;
+    return i;
+  }
+  
  do {
    console.log("I run once regardless.")
  } while(incrementVariable() < integer)
+ 
 }
